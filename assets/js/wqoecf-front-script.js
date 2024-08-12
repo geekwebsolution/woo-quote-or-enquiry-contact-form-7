67882180b7089b930f3e89dca8501841a95d184c
geek-samarth <samarthgeek435@gmail.com>
2024-08-12 11:17:07 +0530

    v3.4.3

--- assets/js/wqoecf-front-script.js
+++ assets/js/wqoecf-front-script.js
@@ -1,24 +1,35 @@
 /** Pass product details to contact form fields */
+var currentRequest = null;
 jQuery("body").on("click", ".wqoecf_enquiry_button", function () {
-    var loading_img_path = jQuery(".wqoecf-pop-up-box").attr("data-loader-path");
-    jQuery("body").append('<div class="wqoecf_loading"><img src="' + loading_img_path + '" class="wqoecf_loader"></div>');
-    var loading = jQuery(".wqoecf_loading");
-    loading.show();
+    
+    var $btn = jQuery(this);
     var product_id    = jQuery(this).attr("data-product-id");
     var product_title = jQuery(this).attr("data-product-title");
     var product_sku   = jQuery(this).attr("data-product-sku");
 
-    jQuery(".wqoecf-pop-up-box .wpcf7 > form")[0].reset();
-    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-id']").val(product_id);
-    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-name']").val(product_title);
-    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-sku']").val(product_sku);
+    $btn.addClass('wqoecf_loading').prepend('<span class="wqoecf_spinner"></span>');
 
-    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-id"]').attr("readonly", true);
-    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-name"]').attr("readonly", true);
-    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-sku"]').attr("readonly", true);
-    loading.remove();
+    currentRequest = jQuery.ajax({
+        type: 'POST',
+        url: wqoecfObj.ajaxurl,
+        data: {
+            'action': 'wqoecf_enquiry_popup'
+        },
+        dataType: 'text',
+        success: function (data) {
+            jQuery('body').append(data);
+            const forms = document.querySelectorAll('.wpcf7 > form');
+            forms.forEach((e) => wpcf7.init(e));    // initialize cf7 after AJAX
 
-    jQuery(".wqoecf-pop-up-box").fadeIn();
+            var popupMain = document.querySelector('.wqoecf-pop-up-box .wpcf7-form');
+            popupMain.scrollTop = 0;
+            
+            wqoecf_update_inputs(product_id, product_title, product_sku);   // Update input values
+
+            jQuery(".wqoecf-pop-up-box").fadeIn();
+            $btn.removeClass('wqoecf_loading').find('.wqoecf_spinner').remove();
+        }
+    });
 });
 
 /** Handler to prevent multiple clicks on submit button  */
@@ -26,6 +37,19 @@ jQuery(document).on("click", ".wqoecf-pop-up-box form.submitting .wpcf7-submit",
     e.preventDefault();
     return false;
 });
+
+/**
+ * Update value of input
+ */
+function wqoecf_update_inputs(id, title, sku) {
+    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-id']").val(id);
+    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-name']").val(title);
+    jQuery("div.wqoecf-pop-up-box .wpcf7 input[name='product-sku']").val(sku);
+
+    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-id"]').attr("readonly", true);
+    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-name"]').attr("readonly", true);
+    jQuery('.wqoecf-pop-up-box div.wpcf7>form input[name="product-sku"]').attr("readonly", true);
+}
 
 /** Function ton hide modal */
 function wqoecf_hide() {
